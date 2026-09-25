882a10418820408f7bb8066b6f095ca5401a3308
Abigail Verholen <abigail.verholen.001@student.uni.lu>
2026-09-25 12:14:37 +0200

    added expressions

--- escaperoom/rooms/base.py
+++ escaperoom/rooms/base.py
@@ -1,20 +1,31 @@
 class Room:
-    def __init__(self, name, doors, items):
+    def __init__(self, name, doors, items, dscr):
         self.name = name
         self.doors = doors
         self.items = items
+        self.dscr = dscr
 
     def arrival(self):
-        return "string you get by entering"
+        str = "You enter the" + self.name
+        if(not self.dscr):
+           str += self.dscr
+        return str
 
     def look(self):
-            return "string you get by looking"
+            str = "You are in the" + self.name +". \n"
+            if(not self.doors.empty):
+                str += "Doors lead to:" + self.get_doors + ". \n"
+            if(not self.items.empty):
+                str += "Items avalible in the room are:" + self.get_items + ". \n"
+                                
+            
+            return str
 
-    def doors(self):
-            return "string of doors"
+    def get_doors(self):
+            return self.doors
 
-    def items(self):
-                return "string of items"
+    def get_items(self):
+                return self.items
 
 base = Room("Intro Lobby", ["soc", "dns", "vault", "malware", "final"],
           [])
